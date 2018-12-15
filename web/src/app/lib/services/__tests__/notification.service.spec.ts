88a170751ee27e05ee9cec7790f4bbc14a37879c
ohager <oliver.hager@dextra-sw.com>
2018-12-15 09:53:27 -0200

    Added notification service test, although the service does nothing yet

---
+++ web/src/app/lib/services/__tests__/notification.service.spec.ts
@@ -0,0 +1,41 @@
+import {inject, TestBed} from '@angular/core/testing';
+import {LoggerService} from '../logger.service';
+import {environment} from '../../../../environments/environment'
+//jest.mock('../../../../environments/environment');
+
+describe('LoggerService', () => {
+    let consoleSpy;
+
+    beforeEach(() => {
+        consoleSpy = jest.spyOn(global.console, 'log');
+        TestBed.configureTestingModule({
+            providers: [
+                LoggerService,
+            ]
+        });
+    });
+
+    afterEach(() => {
+        consoleSpy.mockClear()
+    });
+
+    it('should be created', inject([LoggerService], (service: LoggerService) => {
+            expect(service).not.toBeNull();
+        })
+    );
+
+    it('should log, if environment is not silent', inject([LoggerService], (service: LoggerService) => {
+            environment.silent = false;
+            service.log("Test", "Some message");
+            expect(consoleSpy).toHaveBeenCalled();
+        })
+    );
+
+    it('should not log, if environment is silent', inject([LoggerService], (service: LoggerService) => {
+            environment.silent = true;
+            const spy = jest.spyOn(global.console, 'log');
+            service.log("Test", "Some message");
+            expect(spy).not.toHaveBeenCalled();
+        })
+    );
+});
